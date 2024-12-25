c9540e192a8a0b280362ceb91bfa842132de128f
shezanusman08 <164097373+shezanusman08@users.noreply.github.com>
2024-12-26 02:12:31 +0500

    final

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,14 +10,14 @@ import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemaTypes';
+import {schema} from './sanity/schemaTypes/index'
 import {structure} from "./sanity/structure";
 
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: "6w3noj16",
+  dataset: "production",
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,21 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
+import ImageUrlBuilder from "@sanity/image-url";
 
-import { apiVersion, dataset, projectId } from '../env'
-
+// Initialize the Sanity client
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  apiVersion: "2024-11-24",
+  dataset: "production",
+  projectId: "6w3noj16",
+  useCdn: false,
+});
+
+// Initialize the image URL builder
+const builder = ImageUrlBuilder(client);
+
+// Use 'unknown' instead of 'any', and provide a type guard
+export function urlFor(source: unknown) {
+  if (source && typeof source === "object") {
+    return builder.image(source);
+  }
+  throw new Error("Invalid image source provided.");
+}
